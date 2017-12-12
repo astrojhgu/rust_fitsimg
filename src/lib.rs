05e6725b7b569f1cf2e780c629ff08abc08196d1
Gu Junhua <jhgu@nao.cas.cn>
2017-12-12 23:04:15 +0800

    修改：     src/lib.rs 	修改：     src/main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,94 +16,97 @@ use num_traits::NumCast;
 use ndarray::IntoDimension;
 use ndarray::ArrayD;
 
-
-pub trait TypeToImageType{
-    fn get_img_type()->ImageType;
+pub trait TypeToImageType {
+    fn get_img_type() -> ImageType;
 }
 
-impl TypeToImageType for i8{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for i8 {
+    fn get_img_type() -> ImageType {
         ImageType::SBYTE_IMG
     }
 }
 
-impl TypeToImageType for u8{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for u8 {
+    fn get_img_type() -> ImageType {
         ImageType::BYTE_IMG
     }
 }
 
-impl TypeToImageType for i16{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for i16 {
+    fn get_img_type() -> ImageType {
         ImageType::SHORT_IMG
     }
 }
 
-impl TypeToImageType for u16{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for u16 {
+    fn get_img_type() -> ImageType {
         ImageType::USHORT_IMG
     }
 }
 
-impl TypeToImageType for i32{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for i32 {
+    fn get_img_type() -> ImageType {
         ImageType::LONG_IMG
     }
 }
 
-impl TypeToImageType for u32{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for u32 {
+    fn get_img_type() -> ImageType {
         ImageType::ULONG_IMG
     }
 }
 
-impl TypeToImageType for i64{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for i64 {
+    fn get_img_type() -> ImageType {
         ImageType::LONGLONG_IMG
     }
 }
 
-impl TypeToImageType for f32{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for f32 {
+    fn get_img_type() -> ImageType {
         ImageType::FLOAT_IMG
     }
 }
 
-impl TypeToImageType for f64{
-    fn get_img_type()->ImageType{
+impl TypeToImageType for f64 {
+    fn get_img_type() -> ImageType {
         ImageType::DOUBLE_IMG
     }
 }
 
-pub fn read_img<T>(fname:String, n:usize)->Result<ArrayD<T>>
-where T:Float+NumCast+ReadWriteImage
+pub fn read_img<T>(fname: String, n: usize) -> Result<ArrayD<T>>
+where
+    T: Float + NumCast + ReadWriteImage,
 {
-    let mut fits_file=fitsio::FitsFile::open(fname)?;
-    let hdu=fits_file.hdu(n)?;
-    let shape=
-    match hdu.info {
+    let mut fits_file = fitsio::FitsFile::open(fname)?;
+    let hdu = fits_file.hdu(n)?;
+    let shape = match hdu.info {
         HduInfo::ImageInfo { ref shape, .. } => shape.clone(),
-        _ => return Err(Error::Message("Not image".to_string()))
+        _ => return Err(Error::Message("Not image".to_string())),
     };
 
-    let data=hdu.read_image(&mut fits_file)?;
+    let data = hdu.read_image(&mut fits_file)?;
 
-    match ArrayD::<T>::from_shape_vec(shape.into_dimension(), data){
+    match ArrayD::<T>::from_shape_vec(shape.into_dimension(), data) {
         Ok(x) => Ok(x),
-        Err(_) => Err(Error::Message("err".to_string()))
+        Err(_) => Err(Error::Message("err".to_string())),
     }
 }
 
-pub fn write_img<T>(fname:String, data:&ArrayD<T>) -> Result<()>
-where T:Float+NumCast+ReadWriteImage+TypeToImageType
+pub fn write_img<T>(fname: String, data: &ArrayD<T>) -> Result<()>
+where
+    T: Float + NumCast + ReadWriteImage + TypeToImageType,
 {
-    let mut fits_file=fitsio::FitsFile::create(fname)?;
-    let shape=data.shape();
-    let img_desc=ImageDescription{data_type:T::get_img_type(), dimensions:shape};
+    let mut fits_file = fitsio::FitsFile::create(fname)?;
+    let shape = data.shape();
+    let img_desc = ImageDescription {
+        data_type: <T as TypeToImageType>::get_img_type(),
+        dimensions: shape,
+    };
 
-    let hdu=fits_file.create_image("".to_string(), &img_desc)?;
-    let mut data1=Vec::<T>::new();
-    for x in data.into_iter(){
+    let hdu = fits_file.create_image("".to_string(), &img_desc)?;
+    let mut data1 = Vec::<T>::new();
+    for x in data.into_iter() {
         data1.push(*x);
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate fitsimg;
 extern crate ndarray;
 
-
-
-
-fn main(){
-    let aa=ndarray::arr2(&[[1.0,2.0],[2.0,3.0]]).into_dyn();
+fn main() {
+    let aa = ndarray::arr2(&[[1.0, 2.0], [2.0, 3.0]]).into_dyn();
     fitsimg::write_img("a.fits".to_string(), &aa);
+    let bb = fitsimg::read_img("a.fits".to_string(), 1).unwrap();
+    assert_eq!(bb, aa);
 }
