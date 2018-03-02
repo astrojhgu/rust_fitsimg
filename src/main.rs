175dcabb0240aa200eeb546a6e9b8a7368e52cff
Gu Junhua <jhgu@nao.cas.cn>
2018-03-02 11:04:36 +0800

    修改：     src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@ use std::convert::From;
 
 fn main() {
     //let aa = ndarray::arr2(&[[1.0, 2.0], [2.0, 3.0]]).into_dyn();
-    let mut aa = ndarray::Array2::<f64>::zeros((1024, 512)).into_dyn();
+    let mut aa = ndarray::Array3::<f64>::zeros((8, 16, 32)).into_dyn();
     println!("{:?}", aa.shape());
     println!("{} {}", aa.shape()[0], aa.shape()[1]);
     let mut cnt = 0.0;
     for i in 0..aa.shape()[0] {
         for j in 0..aa.shape()[1] {
-            aa[[i, j]] = (i + j) as f64;
-            cnt = cnt + 1.0;
+            for k in 0..aa.shape()[2] {
+                aa[[i, j, k]] = (i + j + k) as f64;
+                cnt = cnt + 1.0;
+            }
         }
     }
 
